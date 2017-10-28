e2856cbcfda7cdb447e7150fc1e4ee46c5c1b0b7
vdBeren <vdias.savariego@gmail.com>
2017-10-28 12:02:39 -0200

    TODOzinhos

---
+++ Taminha/Assets/_Scripts/ResizeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResizeController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		//TODO: Aplicar Artes: Fantasma, Menino, Balão de Texto
+		//TODO: Aplicar Sons: Música do Minigame, Fantasma Esticando, I'm a boy[?], 
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//TODO: Aplicar código do KENZI de Deform de Mesh e Comparação
+		//TODO: Aplicar Regras do Minigame
+
+
+	}
+}
--- Taminha/Assets/_Scripts/TitleController.cs
+++ Taminha/Assets/_Scripts/TitleController.cs
@@ -8,11 +8,17 @@ namespace Melancia.Taminha {
 
 		// Use this for initialization
 		void Start () {
-			
+			//TODO: Aplicar Artes: Logo do Jogo, Textos, Fechadura, Fantasma
+			//TODO: Aplicar Sons: Som de Fundo, Som de Menu, Som de Deformar Fantasminha
 		}
 		
 		// Update is called once per frame
 		void Update () {
+			
+			//TODO: INTRO com logos da SPJAM, fade, Logo da Melancia
+			//TODO: Transitions de componentes da tela de título, liberando ações ao final.
+			//TODO: Fazer FANTASMA entrar na fechadura e realizar transição para primeira tela de jogo.
+
 
 			if(Input.GetMouseButtonDown(0)){
 				// PRESS ANY BUTTON
